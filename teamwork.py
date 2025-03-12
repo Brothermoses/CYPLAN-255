1112076fcd298644adc62a2900ed63a50f02bb7b
Lisa Tran <lisa.tran@berkeley.edu>
2025-03-12 07:51:40 +0000

    serviceperformance file and read

--- teamwork.py
+++ teamwork.py
@@ -1,10 +1,14 @@
 #Ridership code
 
-# import pandas module  
+# import pandas module 
 import pandas as pd  
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# making dataframe  
+# making dataframes  
 ridership = pd.read_csv("Ridership.csv")  
+service = pd.read_csv("AC_Transit_ServicePerformance.csv")
    
 # output the dataframe 
 print(ridership)
